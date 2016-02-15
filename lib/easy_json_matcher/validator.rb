a4dee00d127f136e571d258c438694a65295e22a
WJD Hamilton <wjdhamilton@hotmail.co.uk>
2016-02-15 15:34:47 +0000

    Users can require values to be set
    EAS-1 #time 90m
    EAS-1 #comment Remember to always convert the arguments to test_node#valid? to JSON before testing
    EAS-1 #comment Positive and Negative tests both passing
    
    Also updated the main test file to the correct package name

--- lib/easy_json_matcher/validator.rb
+++ lib/easy_json_matcher/validator.rb
@@ -1,19 +1,27 @@
 module EasyJSONMatcher
   class Validator
 
-    attr_reader :content, :key
+    attr_reader :content, :key, :required
 
     def initialize(options: {})
       @key = options[:key]
+      @required = options[:required]
     end
 
     def valid?(candidate)
       _set_content(candidate)
+      return false unless _check_required
       _validate
     end
 
     def _set_content(candidate)
       @content = key ? candidate[key.to_s] : candidate
+    end
+
+    def _check_required
+      return true unless required
+      byebug
+      return true if content
     end
   end
 end
