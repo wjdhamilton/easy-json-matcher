c4d9ada9e35ca6338e64228e18572eea3d6f6efd
James Hamilton <wjdhamilton@balnaandevelopments.co.uk>
2016-04-01 13:47:04 +0100

    Changes as per Hound

--- test/custom_validations_test.rb
+++ test/custom_validations_test.rb
@@ -4,23 +4,22 @@ class CustomValidationsTest < ActiveSupport::TestCase
 
   test 'As a user, I want to be able to specify custom validations' do
     test_schema = EasyJSONMatcher::SchemaGenerator.new do |s|
-      s.has_string key: :a_string, opts: { custom_validator: ->(candidate) { "String did not say 'hi'" unless candidate == 'hi' } }
+      string_validator = ->(candidate) { "String did not say 'hi'" unless candidate == 'hi' }
+      s.has_string key: :a_string, opts: { custom_validator: string_validator }
     end.generate_schema
 
     should_not_validate = {
       a_string: 'go away'
     }.to_json
 
-    assert_not(test_schema.valid? should_not_validate)
+    assert_not(test_schema.valid?(should_not_validate))
 
     should_validate = {
       a_string: 'hi'
     }.to_json
 
-
     test_schema.reset!
 
     assert(test_schema.valid?(should_validate), test_schema.get_errors)
-
   end
 end
