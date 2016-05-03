1e907c025422cfbee5b5320f40d0939f56ce94ff
James Hamilton <wjdhamilton@balnaandevelopments.co.uk>
2016-05-03 10:33:52 +0100

    Implemented ValidationStep and Tested
    ValidationStep provides a method of chaining together verification steps
    for validating a value. By creating this chain we can create reusable
    validators which could eventually supercede the Validator class.
    However, for the time being it is probably still best to use
    ValidationSteps within the Validator class. They could certainly be used
    to get rid of the inheritance hierarchy of validator and use composition
    instead.

---
+++ lib/easy_json_matcher/validation_step.rb
@@ -0,0 +1,27 @@
+# The ValidationStep class represents a step in the process of validating a value. Each ValidationStep
+# instance can be chained to another ValidationStep instance in order to produce a defined process for 
+# running validations within a given Validator. A use case for such a procedure is where a value is 
+# required and of a certain type. In this case the validation for required must precede the check for 
+# the type, and validation must cease if the value is nil. 
+module EasyJSONMatcher
+  class ValidationStep
+
+    attr_reader :next_step, :verifier
+
+    def initialize(verify_with:)
+      @verifier = verify_with
+    end
+
+    def check(value:, errors: [])
+      if verifier.call(value, errors) == false || next_step.nil?
+        return errors
+      else
+        return next_step.check(value: value, errors: errors)
+      end
+    end
+
+    def >>(step)
+      @next_step = step
+    end
+  end
+end
---
+++ test/validation_step_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+require "easy_json_matcher/validation_step"
+
+class ValidationStepTest < ActiveSupport::TestCase
+
+  setup do
+    @subject = EasyJSONMatcher::ValidationStep.new(verify_with: ->(value, errors){})
+  end 
+
+  test "it_should_respond_to_check" do
+    assert_respond_to(@subject, :check)
+  end
+
+  test "It should allow the user to chain validation steps together" do
+    head = @subject
+    tail = MiniTest::Mock.new
+    test_value = "hello!"
+    head >> tail
+    tail.expect(:check, nil,[ Hash ])
+    tail.expect(:nil?, false)
+    head.check(value: test_value)
+    tail.verify
+  end
+
+  test "It should send call to the verifier assigned to it" do
+    verifier = MiniTest::Mock.new
+    subject = EasyJSONMatcher::ValidationStep.new(verify_with: verifier)
+    test_value = "verify me"
+    verifier.expect(:call, nil, [ test_value , []])
+    subject.check(value: test_value)
+  end
+
+  test "If the verifier returns :stop the chain will be halted" do
+    verifier = ->(value, errors) { false }
+    middle = EasyJSONMatcher::ValidationStep.new(verify_with: verifier)
+    tail = MiniTest::Mock.new
+    test_value = "verify me"
+    @subject >> middle >> tail
+    @subject.check(value: test_value)
+    # There is no official way to check that something wasn't called in MiniTest, so we'll
+    # just have to assume that since verify passes, then check wasn't called in tail
+    tail.verify
+  end
+
+  test "It should return an array of errors found" do
+    head = EasyJSONMatcher::ValidationStep.new(verify_with: ->(value, errors) { errors << 1 })
+    tail = EasyJSONMatcher::ValidationStep.new(verify_with: ->(value, errors) { errors << 2 })
+    head >> tail
+    assert(head.check(value: "Oh! Mrs. Mogs") == [1,2])
+  end
+end
