0580e15bb1de898af9db3380dde8252317c3eb45
James Hamilton <wjdhamilton@balnaandevelopments.co.uk>
2016-06-24 15:52:54 +0100

    Added plural attribute methods

--- lib/easy_json_matcher/attribute_type_methods.rb
+++ lib/easy_json_matcher/attribute_type_methods.rb
@@ -1,7 +1,5 @@
 module AttributeTypeMethods
 
-
-
     def has_boolean(key:, opts: [])
       has_attribute(key: key, opts: opts + [:boolean])
     end
@@ -26,4 +24,32 @@ module AttributeTypeMethods
       has_attribute(key: key, opts: opts + [:string])
     end
 
+    def has_strings(keys:, opts: [])
+      mass_assign(keys: keys, opts: opts, meth: :has_string)
+    end
+
+    def has_booleans(keys:, opts: [])
+      mass_assign(keys: keys, opts: opts, meth: :has_boolean)
+    end
+
+    def has_numbers(keys:, opts: [])
+      mass_assign(keys: keys, opts: opts, meth: :has_number)
+    end
+
+    def has_objects(keys:, opts: [])
+      mass_assign(keys: keys, opts: opts, meth: :has_object)
+    end
+
+    def has_values(keys:, opts: [])
+      mass_assign(keys: keys, opts: opts, meth: :has_value)
+    end
+
+    def has_dates(keys:, opts: [])
+      mass_assign(keys: keys, opts: opts, meth: :has_date)
+    end
+
+
+    def mass_assign(keys:, opts: [], meth:)
+      keys.each { |k| self.send(meth, { key: k, opts: opts }) }
+    end
 end
